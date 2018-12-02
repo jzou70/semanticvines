93235b0980e815d970d45f480ba83e9dc64bc5e5
Jennifer Zou <jzou70@gmail.com>
2018-12-01 23:03:50 -0500

    init

---
+++ semantic_vectors.py
@@ -0,0 +1,4 @@
+STOCK_PATH = "/Documents/MIT/Fall 2018/UROP/Stock List.xlsx"
+REUTERS_PATH = "/Documents/MIT/Fall 2018/UROP/reuters21578" #folder
+GLOVE_6B_50D_PATH = "/Documents/MIT/Micronotes/Kaeid/glove.6b.50d.txt"
+GLOVE_6B_300D_PATH = "/Documents/MIT/Micronotes/Kaeid/glove.6b.300d.txt"
